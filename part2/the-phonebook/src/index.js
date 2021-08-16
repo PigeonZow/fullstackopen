e704e07412c32cbef538de8f99879de159e91058
PATRICK ZHAO <patrick.zhao@km.kongsberg.com>
2021-08-16 16:51:22 -0700

    exercise 2.11

--- part2/the-phonebook/src/index.js
+++ part2/the-phonebook/src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import axios from 'axios';
-
-const promise = axios.get('http://localhost:3001/persons')
-console.log(promise)
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
